52718d9c793d270002872962e99f38aefa075a00
Elaidzha1940 <eli080293@gmail.com>
2023-08-12 13:34:59 +0300

    added HeadView

--- NeomorphismFitnessApp/Fitness/Fitness.swift
+++ NeomorphismFitnessApp/Fitness/Fitness.swift
@@ -18,41 +18,13 @@ struct Fitness: View {
           
             HeadView(name: "Elijah")
             
-            Text("Elijah")
-                .font(.largeTitle)
+           Spacer()
         }
         .padding()
-        
+        .background(Color(red: 0.922, green: 0.925, blue: 0.941))
     }
 }
 
-struct HeadView: View {
-    
-    let name: String
-    
-    var body: some View {
-        
-        HStack(alignment: .top) {
-            VStack(alignment: .leading, spacing: 15) {
-                Text("Morning\n\(name)")
-                    .font(.system(size: 30, weight: .semibold, design: .serif))
-                    .multilineTextAlignment(.leading)
-                
-                Text("Lets get started")
-                    .font(.system(size: 20, weight: .regular, design: .serif))
-            }
-            Spacer()
-            
-            Image("profile")
-                .resizable()
-                .scaledToFit()
-                .background(.gray)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .frame(width: 50)
-        }
-        .foregroundColor(.accentColor)
-    }
-}
 
 struct Fitness_Previews: PreviewProvider {
     static var previews: some View {
---
+++ NeomorphismFitnessApp/Fitness/HeadView.swift
@@ -0,0 +1,39 @@
+//  /*
+//
+//  Project: NeomorphismFitnessApp
+//  File: HeadView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 12.08.2023
+//
+//  */
+
+import Foundation
+import SwiftUI
+
+struct HeadView: View {
+    
+    let name: String
+    
+    var body: some View {
+        
+        HStack(alignment: .top) {
+            VStack(alignment: .leading, spacing: 15) {
+                Text("Morning\n\(name)")
+                    .font(.system(size: 30, weight: .semibold, design: .serif))
+                    .multilineTextAlignment(.leading)
+                
+                Text("Lets get started")
+                    .font(.system(size: 20, weight: .regular, design: .serif))
+            }
+            Spacer()
+            
+            Image("profile")
+                .resizable()
+                .scaledToFit()
+                .background(.gray)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .frame(width: 50)
+        }
+        .foregroundColor(.accentColor)
+    }
+}
