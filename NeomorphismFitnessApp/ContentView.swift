24f22bd7ed1ad8916f35623bf371685ef97882d5
Elaidzha1940 <eli080293@gmail.com>
2023-08-12 11:08:21 +0300

    NeomorphismFitnessApp

---
+++ NeomorphismFitnessApp/ContentView.swift
@@ -0,0 +1,28 @@
+//  /*
+//
+//  Project: NeomorphismFitnessApp
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 12.08.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+          
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NeomorphismFitnessApp/NeomorphismFitnessAppApp.swift
@@ -0,0 +1,21 @@
+//  /*
+//
+//  Project: NeomorphismFitnessApp
+//  File: NeomorphismFitnessAppApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 12.08.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+@main
+struct NeomorphismFitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
